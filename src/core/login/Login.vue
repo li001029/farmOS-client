e263936315978dcd8475235331999cffcb2ee8bd
Jiafeng Li <li001029@umn.edu>
2021-07-27 15:03:35 -0500

    added save username and password

--- src/core/login/Login.vue
+++ src/core/login/Login.vue
@@ -1,19 +1,17 @@
 <template>
   <farm-main :paddingTop="['xl', 'xxl']" :paddingX="['m', 'xl', 'xxl']">
- 
-     
-      <select v-model="farmosUrl">
-  <option disabled value="">Choose a server</option>
-  <option>peritusag.farmos.net</option>
-  <option>farmos2.peritusag.com</option>
-   </select>
-  <br>   <br>  
+    <select v-model="farmosUrl">
+      <option disabled value="">Choose a server</option>
+      <option>peritusag.farmos.net</option>
+      <option>farmos2.peritusag.com</option>
+    </select>
+    <br />
+    <br />
 
     <div class="input-group">
       <div class="input-group-prepend">
         <span class="input-group-text">https://</span>
-
-      </div>     
+      </div>
       <input
         v-model="farmosUrl"
         :placeholder="$t('Enter your farmOS URL')"
@@ -22,11 +20,11 @@
         autocomplete="url"
         class="form-control"
         v-on:input="checkValues"
-      >
-
+      />
     </div>
-    <br>
-    <div class="input-group">
+
+    <br />
+    <div class="input-group" id="app">
       <input
         v-model="username"
         :placeholder="$t('Enter your username')"
@@ -34,9 +32,9 @@
         autocomplete="username"
         class="form-control"
         v-on:input="checkValues"
-      >
+      />
     </div>
-    <br>
+    <br />
     <div class="input-group">
       <input
         v-model="password"
@@ -45,12 +43,12 @@
         autocomplete="current-password"
         class="form-control"
         v-on:input="checkValues"
-      >
+      />
     </div>
-    <br>
+    <br />
     <div class="input-group login-submit">
       <div v-if="authPending">
-        <icon-spinner/>
+        <icon-spinner />
       </div>
       <button
         v-else
@@ -60,51 +58,129 @@
         class="btn btn-success btn-navbar"
         type="button"
       >
-        {{ $t('Submit credentials')}}
+        {{ $t("Submit credentials") }}
       </button>
     </div>
-    <br>
-    <p style="textAlign: center">
-      {{ $t('Need a server? Check out')}}
-      <a href="https://farmos.org/hosting/">{{ $t('hosting options')}}</a>.
+    <br />
+    <p style="textalign: center">
+      {{ $t("Need a server? Check out") }}
+      <a href="https://farmos.org/hosting/">{{ $t("hosting options") }}</a
+      >.
     </p>
 
+    <!-- ======================== -->
+    <!-- ======================== -->
+    <!-- ======================== -->
 
-
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <div id="app">
+      储存账号:<input v-model="name" />
+      <br />
+      储存密码:
+      <input type="password" v-model="pw" />
+      <p />
+      <button @click="persist">保存登陆信息</button>
+      <button @click="persist2">删除缓存</button>
+    </div>
+    <!-- ======================== -->
+    <!-- ======================== -->
+    <!-- ======================== -->
   </farm-main>
 </template>
 
 <script>
 export default {
-  name: 'Login',
+  name: "Login",
+  localStorage: {
+    str: "",
+    num: "",
+    name: "",
+    pw: "",
+  },
   data() {
-    return {
-      valuesEntered: false,
-      authPending: false,
-      username: '',
-      password: '',
-      farmosUrl: '',
-    };
+    if (localStorage.name) {
+      console.log("here is username : " + localStorage.name);
+      return {
+        valuesEntered: false,
+        authPending: false,
+        username: localStorage.name,
+        // password: localStorage.password,
+        farmosUrl: "",
+        password: localStorage.pw,
+        newKeyValueObj: {
+          key: "",
+          value: "",
+        },
+        newValueToArr: "",
+      };
+    } else {
+      // console.log("localStorage.name: " + localStorage.name);
+      return {
+        valuesEntered: false,
+        authPending: false,
+        username: "",
+        password: "",
+        farmosUrl: "",
+
+        newKeyValueObj: {
+          key: "",
+          value: "",
+        },
+        newValueToArr: "",
+      };
+    }
+
+    // };
+  },
+  mounted() {
+    if (localStorage.name) this.name = localStorage.name;
+    if (localStorage.pw) this.pw = localStorage.pw;
+    console.log("start mounted()  now");
+    console.log("this.name: " + this.name);
+    console.log("this.pw: " + this.pw);
+    console.log("end  mounted() ");
+    console.log();
+    console.log();
   },
 
   methods: {
+    // =====================
+    // =====================
+
+    persist() {
+      localStorage.name = this.name;
+      localStorage.pw = this.pw;
+      console.log("start  persist() now");
+      console.log("this.name: " + this.name);
+      console.log("this.pw: " + this.pw);
+      console.log("end  persist()");
+      console.log();
+      console.log();
+    },
+
+    persist2() {
+      localStorage.name = "";
+      localStorage.pw = "";
+    },
     checkValues() {
-      const urlIsValid = process.env.NODE_ENV === 'development' || this.username !== '';
-      const usernameIsValid = this.username !== '';
-      const passwordIsValid = this.password !== '';
+      const urlIsValid =
+        process.env.NODE_ENV === "development" || this.username !== "";
+      const usernameIsValid = this.username !== "";
+      const passwordIsValid = this.password !== "";
       if (urlIsValid && usernameIsValid && passwordIsValid) {
         this.valuesEntered = true;
       }
     },
 
-
     submitCredentials() {
       const payload = {
- 
         farmosUrl: this.farmosUrl,
- 
-
-
         username: this.username,
         password: this.password,
         router: this.$router,
@@ -112,29 +188,27 @@ export default {
 
       this.authPending = true;
 
-      this.$store.dispatch('authorize', payload)
+      this.$store
+        .dispatch("authorize", payload)
         .then(() => {
           this.authPending = false;
-          this.$store.commit('setLoginStatus', true);
-          return this.$store.dispatch('updateFieldModules', this.$router);
+          this.$store.commit("setLoginStatus", true);
+          return this.$store.dispatch("updateFieldModules", this.$router);
         })
-        .then(res => this.$store.dispatch('updateUserAndSiteInfo', res))
-        .then(res => this.$store.dispatch('updateFarmResources', res));
+        .then((res) => this.$store.dispatch("updateUserAndSiteInfo", res))
+        .then((res) => this.$store.dispatch("updateFarmResources", res));
     },
-
   },
   created() {
-    this.farmosUrl = localStorage.getItem('host')?.replace(/(^\w+:|^)\/\//, '') || '';
+    this.farmosUrl =
+      localStorage.getItem("host")?.replace(/(^\w+:|^)\/\//, "") || "";
   },
 };
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
- .login-submit {
-   justify-content: center;
- }
-
+.login-submit {
+  justify-content: center;
+}
 </style>
